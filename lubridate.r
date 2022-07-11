c50827169b9a910675c4f2ef111f006163a87a9c
Minyoung Kim <93497667+min02yam@users.noreply.github.com>
2022-07-11 20:38:20 +0900

    Update lubridate.r

--- lubridate.r
+++ lubridate.r
@@ -19,3 +19,9 @@ akl_hourly %>% select(date, time, datetime)
 # datetime에 따른 temp (연간 변화와 비교 가능)
 ggplot(akl_hourly, aes(x = datetime, y = temperature)) +
   geom_line()
+
+##################################
+#라운드하기
+#round_date(), ceiling_date(), floor_date()
+# Create day_hour, datetime rounded down to hour
+df %>% mutate(day_hour= floor_date(datetime, unit="hour"))
